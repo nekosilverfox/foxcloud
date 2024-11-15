5d0b7f804a74ab7fc7932bf80da214f939995a15
冰糖雪狸 <weidufox@gmail.com>
2024-11-15 18:46:20 +0300

    增加 开机启动 fdfs 服务

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# 启动 fdfs 服务
+fdfs_trackerd /etc/fdfs/tracker.conf start
+fdfs_storaged /etc/fdfs/storage.conf start
+
 # 启动 PostgreSQL 服务
 service postgresql start
 
@@ -9,4 +13,5 @@ redis-server /root/foxcloudservers/foxcloud/config/redis.conf
 
 # 启动 SSH 服务
 /usr/sbin/sshd -D
+
 
