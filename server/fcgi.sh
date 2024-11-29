d14dd1a6f7be7ea43020b001edc7f74093c79f7b
冰糖雪狸 <weidufox@gmail.com>
2024-11-30 01:34:33 +0300

    增加 mysql dev 库缺少的问题

--- server/fcgi.sh
+++ server/fcgi.sh
@@ -51,24 +51,31 @@ if [ "$START" -eq 1 ];then
     # 登录
     echo -n "登录："
     spawn-fcgi -a 127.0.0.1 -p 10000 -f ./bin_cgi/login
+
     # 注册
     echo -n "注册："
     spawn-fcgi -a 127.0.0.1 -p 10001 -f ./bin_cgi/register
+
     # 上传文件
     echo -n "上传："
     spawn-fcgi -a 127.0.0.1 -p 10002 -f ./bin_cgi/upload
+
     # MD5秒传
     echo -n "MD5："
     spawn-fcgi -a 127.0.0.1 -p 10003 -f ./bin_cgi/md5
+
     # 我的文件
     echo -n "MyFile："
     spawn-fcgi -a 127.0.0.1 -p 10004 -f ./bin_cgi/myfiles
+
     # 分享删除文件
     echo -n "DealFile："
     spawn-fcgi -a 127.0.0.1 -p 10005 -f ./bin_cgi/dealfile
+
     # 共享文件列表
     echo -n "ShareList："
     spawn-fcgi -a 127.0.0.1 -p 10006 -f ./bin_cgi/sharefiles
+    
     # 共享文件pv字段处理、取消分享、转存文件
     echo -n "DealShare："
     spawn-fcgi -a 127.0.0.1 -p 10007 -f ./bin_cgi/dealsharefile
