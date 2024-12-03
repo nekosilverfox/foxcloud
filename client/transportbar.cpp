8b600c0f5387d8bac11a492e136d5a0cb914c33f
冰糖雪狸 <weidufox@gmail.com>
2024-12-03 14:44:40 +0300

    实现 TransformBar 的实现

--- client/transportbar.cpp
+++ client/transportbar.cpp
@@ -1,14 +1,54 @@
 #include "transportbar.h"
 #include "ui_transportbar.h"
 
-TransportBar::TransportBar(QWidget *parent)
+TransportBar::TransportBar(QWidget *parent, unsigned int maxValue)
     : QWidget(parent)
     , ui(new Ui::TransportBar)
 {
     ui->setupUi(this);
+
+    _max_value = maxValue;
 }
 
 TransportBar::~TransportBar()
 {
     delete ui;
+}
+
+/**
+ * @brief TransportBar::setFileName 设置问价名
+ * @param name 文件名
+ */
+void TransportBar::setFileName(QString name)
+{
+    if (name.isEmpty())
+    {
+        qWarning() << "File name is empty";
+    }
+
+    ui->lbFileName->setText(name);
+}
+
+/**
+ * @brief TransportBar::setValue 设置当前值
+ * @param value
+ */
+void TransportBar::setValue(unsigned int value)
+{
+    ui->progressBar->setValue(value);
+    ui->lbPercent->setText(QString::number(value / _max_value, 'f', 2));
+}
+
+/**
+ * @brief TransportBar::setMaxValue 设置最大值
+ * @param value
+ */
+void TransportBar::setMaxValue(unsigned int value)
+{
+    if (0 == value)
+    {
+        qWarning() << "Max value is 0";
+    }
+    _max_value = value;
+    ui->progressBar->setMaximum(_max_value);
 }
--- client/transportbar.h
+++ client/transportbar.h
@@ -12,11 +12,17 @@ class TransportBar : public QWidget
     Q_OBJECT
 
 public:
-    explicit TransportBar(QWidget *parent = nullptr);
+    explicit TransportBar(QWidget *parent = nullptr, unsigned int maxValue = 0);
     ~TransportBar();
+
+    void setFileName(QString name = "FILENAME");
+    void setValue(unsigned int value = 0);
+    void setMaxValue(unsigned int value = 0);
 
 private:
     Ui::TransportBar *ui;
+
+    unsigned int _max_value;
 };
 
 #endif // TRANSPORTBAR_H
