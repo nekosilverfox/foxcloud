f50b8788f2cba0d457abe50792ff74ab0b6c5d49
冰糖雪狸 <weidufox@gmail.com>
2024-12-12 22:24:03 +0300

    增加 全局字体并移除 history 页

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QLoggingCategory>
+#include <QFontDatabase>
 
 #include "windowmanager.h"
 #include "common/config.h"
@@ -8,12 +9,23 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setFont(QFont(":/font/BM_JUA.ttf", 14, QFont::Normal, false));
     a.setStyleSheet(
         "QWidget { color: rgb(11, 54, 79); }"
         "QMessageBox { background-color: rgb(230, 240, 250); }"
         );
     a.setWindowIcon(QIcon(":/img/foxcloud-logo-name.svg"));
+
+    /* 加载自定义字体文件 */
+    int fontId = QFontDatabase::addApplicationFont(":/font/BM_JUA.ttf");
+    if (fontId == -1)
+    {
+        qDebug() << "Failed to load custom font";
+    }
+    QString fontFamily = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    qDebug() << "Set font family as" << fontFamily;
+
+    QFont customFont(fontFamily, 14, QFont::Normal);
+    a.setFont(customFont);
 
     /* 初始化 Logger */
     // QLoggingCategory::setFilterRules("*.debug=false;");  // 设置日志级别
